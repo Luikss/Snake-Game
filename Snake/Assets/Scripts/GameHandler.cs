32199bb9cd5d187634a982134fe1b11cfd20abd7
Andre Luige <78085382+Luikss@users.noreply.github.com>
2021-02-06 14:45:50 +0200

    Add files via upload
    Desktop app didn't upload it for some reason

---
+++ Snake/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+    [SerializeField] private Snake snake;
+    private LevelGrid levelGrid;
+
+    void Start() {
+        levelGrid = new LevelGrid(21,21);
+        snake.Setup(levelGrid);
+        levelGrid.Setup(snake);
+    }
+
+    void Update() {
+    }
+}

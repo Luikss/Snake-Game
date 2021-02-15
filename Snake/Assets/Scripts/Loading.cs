1003a165025b92b92b7236e9853fb3a444d6628f
Andre Luige <78085382+Luikss@users.noreply.github.com>
2021-02-15 16:30:27 +0200

    code

---
+++ Snake/Assets/Scripts/Loading.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Loading {
+ 
+    public enum Scene {
+        Main,
+    }
+
+    public static void Load(Scene scene) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene.ToString());
+    }
+}

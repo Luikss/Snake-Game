d98bbb8a77e15396a7549cceb0c017ad3483612b
Andre Luige <78085382+Luikss@users.noreply.github.com>
2021-02-18 13:40:16 +0200

    Code

---
+++ Snake/Assets/Scripts/PauseMenuButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuButtons : MonoBehaviour {
+   
+    public void PauseToMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void PauseToResume() {
+        PauseWindow.HideStatic();
+        Time.timeScale = 1f;
+    }
+}
